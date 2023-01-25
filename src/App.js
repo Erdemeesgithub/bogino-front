94c815f145b55c87a3bddd8be3bd74c19ea994ff
Erdem <110883062+UrCheryl@users.noreply.github.com>
2023-01-25 23:01:15 +0800

    s

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div>
       <BrowserRouter>
         <Routes>
-          <Route path="/home" element={<Home/>}/>
+          <Route path="/" element={<Home/>}/>
           <Route path="/login" element={<Login/>}/>
           <Route path="/signup" element={<Signup/>}/>
         </Routes>
